66f6881ea68e70b954ee6668035476863274b647
Silkou Maxim <maxim.silkou.2002@gmail.com>
2023-12-06 10:19:36 +0300

    add property listing cotroller and change default endpoint

---
+++ BookingProject/Controllers/PropertyListingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingProject.Controllers
+{
+    public class PropertyListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult ListAvailable(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BookingProject/Program.cs
+++ BookingProject/Program.cs
@@ -6,12 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -38,7 +41,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=PropertyListing}/{action=ListAll}");
 app.MapRazorPages();
 
 app.Run();
