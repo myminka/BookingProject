61e331c03362fea566e3ee3d4302df56c22621b5
Silkou Maxim <maxim.silkou.2002@gmail.com>
2023-12-06 11:45:27 +0300

    8. update list properties

--- BookingProject/Controllers/PropertyListingController.cs
+++ BookingProject/Controllers/PropertyListingController.cs
@@ -6,45 +6,7 @@ namespace BookingProject.Controllers
 {
     public class PropertyListingController : Controller
     {
-        private static List<PropertyListingViewModel> properties = new()
-        {
-            new ()
-            {
-                Id = 1,
-                Name = "Rose Cottage",
-                Blurb = "Beautiful cottage on the Cornwall coast",
-                Location = "Cornwall",
-                NumberOfBedrooms = 3,
-                CostPerNight = 350m
-            },
-            new ()
-            {
-                Id = 2,
-                Name = "Safron House",
-                Blurb = "Stately home on the Devon moores",
-                Location = "Devon",
-                NumberOfBedrooms = 7,
-                CostPerNight = 730m
-            },
-            new ()
-            {
-                Id = 3,
-                Name = "Cozy Cottage",
-                Blurb = "A charming cottage in the countryside.",
-                Location = "Rural Area",
-                NumberOfBedrooms = 2,
-                CostPerNight = 80.00m
-            },
-            new ()        
-            {
-                Id = 4,
-                Name = "Modern Apartment",
-                Blurb = "Sleek and modern apartment in the heart of the city.",
-                Location = "City Center",
-                NumberOfBedrooms = 1,
-                CostPerNight = 120.00m
-            },
-        };
+        private static List<PropertyListingViewModel> properties = new();
 
         public PropertyListingController() 
         {
